8cb234497634202eda32bdaaaea141cd0be88774
Alexander Sakhaev <alexander_sakhaev@mail.ru>
2019-02-20 16:28:36 +0300

    ui fix

--- ui/app/components/pages/confirm-widget/confirm-widget.component.js
+++ ui/app/components/pages/confirm-widget/confirm-widget.component.js
@@ -8,7 +8,7 @@ export default class ConfirmWidget extends Component {
 
     //TODO: may be it's well to use something like DOMPurify for XSS protection
     return (
-      <div class="confirm-page-container-content" style="padding: 16px;">
+      <div className="confirm-page-container-content" style={{padding: '16px'}}>
         <style type="text/css" dangerouslySetInnerHTML={{ __html: style }} />
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </div>
